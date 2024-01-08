3fb701da7855b092de218a82da65b1800c783bfd
Ankit Sharma <ankitsharma290101@gmail.com>
2024-01-08 22:00:31 +0530

    components

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = ({ width = "100%" }) => {
+  return (
+    <div>
+      <img src="" style={{ width }} alt="logo" />
+    </div>
+  );
+};
+
+export default Logo;
